684fd5bb2d545367fb29944d67e13e46977bdd6f
Hamza Mekouar <mekouar.hamza1@gmail.com>
2025-07-27 16:34:23 +0100

    add required Gradle dependencies and enable DataBinding V2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,14 +32,33 @@ android {
     buildFeatures{
         viewBinding = true
     }
+    dataBinding{
+        enable = true
+    }
 }
 
 dependencies {
+    // AndroidX UI & Support
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Volley (Network / API)
+    implementation(libs.volley)
+
+    // Lifecycle Components (ViewModel & LiveData)
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.lifecycle.runtime)
+
+    // Gson (For parsing JSON)
+    implementation(libs.gson)
+
+    // Unit Testing
     testImplementation(libs.junit)
+
+    // Android Instrumentation Tests
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
